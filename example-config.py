f0d81333ed28dcc2f132714ad9343d5520b65413
thisisathrowawayaccount2 <thatguynamedskinner@gmail.com>
2026-06-21 19:57:10 -0500

    added some configurable manual variables for scoring structures

--- example-config.py
+++ example-config.py
@@ -1,3 +1,11 @@
 # Substitute lat and long here to hard code them into the program
 HOME_LATITUDE = 000000
 HOME_LONGITUDE = -00000
+
+# Configurable values for alt and distance
+MAX_TRACKING_DISTANCE = 25.0
+MAX_SCORING_ALTITUDE = 40000.0
+
+# Configurable values for scoring weights
+DISTANCE_WEIGHT = 0.70
+ALTITUDE_WEIGHT = 0.30
